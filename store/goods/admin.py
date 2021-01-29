68e7c13aba0e0e257b2a42c08257594b3ec8b6c4
Akbar <akbarmenglimuratov@gmail.com>
2021-01-29 21:18:11 +0500

    added manu and goodspic models

--- store/goods/admin.py
+++ store/goods/admin.py
@@ -3,10 +3,15 @@ from goods import models
 
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
-	list_display = ['id', 'name']
+	list_display = ['id', 'name', 'slug']
 	prepopulated_fields = {'slug': ('name',)}
 
 @admin.register(models.Goods)
-class CategoryAdmin(admin.ModelAdmin):
-	list_display = ['id', 'name']
+class GoodsAdmin(admin.ModelAdmin):
+	list_display = ['id', 'name', 'slug']
+	prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(models.Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+	list_display = ['id', 'name', 'slug']
 	prepopulated_fields = {'slug': ('name',)}
--- store/goods/models.py
+++ store/goods/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class BaseNameSlug(models.Model):
 	name = models.CharField(max_length = 250)
-	slug = models.CharField(max_length = 250)
+	slug = models.SlugField(max_length = 250)
 
 	class Meta:
 		abstract = True
@@ -20,14 +20,24 @@ class Category(BaseNameSlug):
 		return self.name
 
 class Manufacturer(BaseNameSlug):
-	pass
+	
+	def __str__(self):
+		return self.name
 
 class Goods(BaseNameSlug):
 
 	category = models.ForeignKey(
-		Category, 
-		on_delete = models.CASCADE, 
+		Category,
+		on_delete = models.SET_NULL,
+		null = True,
 		related_name = "goods_category")
 
 	def __str__(self):
 		return self.name
+
+def goods_pic_upload(instance, filename):
+	return "movie_shots/{0}/{1}".format(instance.name,filename)
+class GoodsPicture(models.Model):
+
+	image = models.ImageField(upload_to = goods_pic_upload)
+	goods = models.ForeignKey(Goods, on_delete = models.CASCADE, related_name = "goods_picture")
